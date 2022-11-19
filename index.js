b45e4e1444ee75ba4a44d1752fda0f47d0c0a8f2
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-11-19 16:28:51 +0400

    in process

---
+++ index.js
@@ -0,0 +1,10 @@
+function clickMe() {
+    let catNameInput = document.getElementById('catName');
+    console.log(catNameInput.value);
+}
+
+function changeMe() {
+    let catImage = document.getElementById ('catImage');
+    catImage.src = "Fotoram.io (3).jpg";
+}
+
