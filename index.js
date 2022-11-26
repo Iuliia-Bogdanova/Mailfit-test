153bbe9a7702b2c247f34129e33e5daa2bad2f6f
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-11-26 18:13:26 +0400

    doing

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-function clickMe() {
-    let catNameInput = document.getElementById('catName');
-    console.log(catNameInput.value);
-}
-
 function changeMe() {
     let catImage = document.getElementById ('catImage');
     catImage.src = "Fotoram.io (3).jpg";
 }
 
+function clickMe() {
+    let catNameInput = document.getElementById('catName');
+    console.log(catNameInput.value);
+}
+
+class Cat {
+    constructor(catName, breed, gender, ster, date, feed, comment) {
+        this.catName = catName;
+        this.breed = breed;
+        this.gender = gender;
+        this.ster = ster;
+        this.date = date;
+        this.feed = feed;
+        this.comment = comment;
+    }
+
+    sendResult() {
+        console.log(`Это ${this.name}, он ${this.breed}`);
+    }
+}
+
+let cat = new Cat();
+
+console.log(sendResult);
